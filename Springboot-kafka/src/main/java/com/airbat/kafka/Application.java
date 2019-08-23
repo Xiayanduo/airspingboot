161f4a6b385f9fabe0667c1e9b9eb74ad4f069f7
whj <676897134@qq.com>
2019-08-23 16:33:19 +0800

    kafka 完成

--- Springboot-kafka/src/main/java/com/airbat/kafka/Application.java
+++ Springboot-kafka/src/main/java/com/airbat/kafka/Application.java
@@ -1,5 +1,8 @@
 package com.airbat.kafka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class Application {
 
--- Springboot-kafka/src/main/java/com/airbat/kafka/controller/KafkaProducerController.java
+++ Springboot-kafka/src/main/java/com/airbat/kafka/controller/KafkaProducerController.java
@@ -1,15 +1,23 @@
 package com.airbat.kafka.controller;
 
+
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/")
 public class KafkaProducerController {
 
-    @Autowired
+    @Resource
     private KafkaTemplate<String, String> kafkaTemplate;
 
     @RequestMapping("send")
     public String send(String msg){
-        kafkaTemplate.send("test_topic", msg);
+        kafkaTemplate.send("test_topic", "hello kafka word!");
         return "success";
     }
 
